17fc4d2b5d1f640618207ae3ea0f2e6887833c36
John Watson <john.watson@gmx.co.uk>
2016-10-01 17:16:19 +0100

    Upgraded to latest elm-test
    and reformatted

--- src/Json.elm
+++ src/Json.elm
@@ -15,148 +15,195 @@ import Combine.Num exposing (..)
 import String exposing (fromList)
 import Dict exposing (Dict, fromList)
 import JsonData exposing (..)
---import Debug exposing (..)
 
+
+--import Debug exposing (..)
 {- grammar
 
-    value      ::= obj | arr | stringLiteral | floatingPointNumber | "null" | "true" | "false"
-    obj        ::= "{" [members] "}"
-    arr        ::= "[" [values] "]"
-    members    ::= member {"," member }
-    member     ::= stringLiteral ":" value
-    values     ::= value {"," value }
+   value      ::= obj | arr | stringLiteral | floatingPointNumber | "null" | "true" | "false"
+   obj        ::= "{" [members] "}"
+   arr        ::= "[" [values] "]"
+   members    ::= member {"," member }
+   member     ::= stringLiteral ":" value
+   values     ::= value {"," value }
 
 -}
 
 
-{-| a parse error with context -} 
+{-| a parse error with context
+-}
 type alias ParseError =
-  {  msgs : List String
-  ,  input : String
-  ,  position : Int
-  }
+    { msgs : List String
+    , input : String
+    , position : Int
+    }
+
+
 
 {- a transient member of a JSON object -}
+
+
 type alias Member =
-  (String, JValue)
+    ( String, JValue )
+
 
 quotedString : Parser String
 quotedString =
-  string "\"" *> regex "(\\\\\"|[^\"\n])*" <* string "\"" 
-    <?> "quoted string"
+    string "\""
+        *> regex "(\\\\\"|[^\"\n])*"
+        <* string "\""
+        <?> "quoted string"
+
 
 jstring : Parser JValue
-jstring = 
-   JString <$> quotedString
+jstring =
+    JString
+        <$> quotedString
         <?> "jstring"
 
+
 jnumber : Parser JValue
-jnumber = 
-    JNumber <$> 
-      ( choice
-          [ float
-          , integer
-          ]
-      )
+jnumber =
+    JNumber
+        <$> (choice
+                [ float
+                , integer
+                ]
+            )
         <?> "jnumber"
+
 
 integer : Parser Float
 integer =
-  Basics.toFloat <$> int
+    Basics.toFloat <$> int
+
 
 jbool : Parser JValue
-jbool = 
-   choice
-     [ jtrue
-     , jfalse
-     ]
-      <?> "jbool"
+jbool =
+    choice
+        [ jtrue
+        , jfalse
+        ]
+        <?> "jbool"
+
 
 jtrue : Parser JValue
-jtrue = JBool True <$ string "true"
+jtrue =
+    JBool True <$ string "true"
+
 
 jfalse : Parser JValue
-jfalse = JBool False <$ string "false"
+jfalse =
+    JBool False <$ string "false"
+
 
 jnull : Parser JValue
-jnull = JNull <$ string "null"
+jnull =
+    JNull <$ string "null"
+
 
 jobj : Parser JValue
 jobj =
-   rec <| \() -> 
-      (JObject << Dict.fromList) <$> (braces jmembers)
-         <?> "jobj"
+    rec <|
+        \() ->
+            (JObject << Dict.fromList)
+                <$> (braces jmembers)
+                <?> "jobj"
+
 
 jarr : Parser JValue
 jarr =
-   rec <| \() -> 
-     JArray <$> (brackets jvaluesOrEmpty)
-       <?> "jarr"
+    rec <|
+        \() ->
+            JArray
+                <$> (brackets jvaluesOrEmpty)
+                <?> "jarr"
+
 
 jmembers : Parser (List Member)
-jmembers = 
-  rec <| \() -> 
-    sepBy1 (char ','<* whiteSpace) (whiteSpace *> jmember)
-      <?> "jmembers"
+jmembers =
+    rec <|
+        \() ->
+            sepBy1 (char ',' <* whiteSpace) (whiteSpace *> jmember)
+                <?> "jmembers"
+
 
 jmember : Parser Member
-jmember = 
-  rec <| \() -> 
-    (,) <$> quotedString <*> ((char ':') *> (whiteSpace *> jvalue))
-      <?> "jmember"
+jmember =
+    rec <|
+        \() ->
+            (,)
+                <$> quotedString
+                <*> ((char ':') *> (whiteSpace *> jvalue))
+                <?> "jmember"
+
 
 jvaluesOrEmpty : Parser (List JValue)
 jvaluesOrEmpty =
-  rec <| \() -> 
-    jvalues `or` empty
+    rec <|
+        \() ->
+            jvalues `or` empty
+
 
 empty : Parser (List JValue)
 empty =
-  [] <$ whiteSpace
+    [] <$ whiteSpace
+
 
 jvalues : Parser (List JValue)
-jvalues = 
-  rec <| \() -> 
-    sepBy1 (char ',' <* whiteSpace) (whiteSpace *> jvalue)
-      <?> "jvalues"
+jvalues =
+    rec <|
+        \() ->
+            sepBy1 (char ',' <* whiteSpace) (whiteSpace *> jvalue)
+                <?> "jvalues"
+
 
 jvalue : Parser JValue
 jvalue =
-  let 
-    value =
-      rec <| \() -> 
-        choice
-          [ jobj
-          , jarr
-          , jstring
-          , jnumber
-          , jnull
-          , jbool
-          ]
-  in    
-    -- log "jvalue" <$>
-      (value <* whiteSpace
-        <?> "jvalue")
+    let
+        value =
+            rec <|
+                \() ->
+                    choice
+                        [ jobj
+                        , jarr
+                        , jstring
+                        , jnumber
+                        , jnull
+                        , jbool
+                        ]
+    in
+        -- log "jvalue" <$>
+        (value
+            <* whiteSpace
+            <?> "jvalue"
+        )
+
 
 
 -- low level parsers
 -- possible whitespace
+
+
 whiteSpace : Parser String
-whiteSpace = regex "[ \t\r\n]*"
+whiteSpace =
+    regex "[ \t\x0D\n]*"
+
+
 
 {- just for debugging
-restOfInput : Parser (List Char)
-restOfInput = many anyChar
+   restOfInput : Parser (List Char)
+   restOfInput = many anyChar
 -}
 
 
-{-| entry point - Parse some JSON -}
+{-| entry point - Parse some JSON
+-}
 parse : String -> Result.Result ParseError JValue
 parse s =
-  case Combine.parse jvalue s of
-    (Ok n, _) ->
-      Ok n
+    case Combine.parse jvalue s of
+        ( Ok n, _ ) ->
+            Ok n
 
-    (Err msgs, ctx) ->
-      Err { msgs = msgs, input = ctx.input, position = ctx.position }
-
+        ( Err msgs, ctx ) ->
+            Err { msgs = msgs, input = ctx.input, position = ctx.position }
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -6,92 +6,95 @@ import Samples exposing (..)
 import Json exposing (..)
 import Debug exposing (..)
 
+
 expectParses : String -> Expectation
 expectParses s =
-   let
-      result = Json.parse s 
-   in case result of
-     Err e ->
-       Expect.fail  (toString e)
-     _ -> 
-       Expect.pass
+    let
+        result =
+            Json.parse s
+    in
+        case result of
+            Err e ->
+                Expect.fail (toString e)
+
+            _ ->
+                Expect.pass
 
 
 basicTests : Test
 basicTests =
     describe "Basic tests"
-      [
-        test "true" <|
+        [ test "true" <|
             \() ->
-                expectParses("true")
-      , test "false" <|
+                expectParses ("true")
+        , test "false" <|
             \() ->
-                expectParses("false")
-      , test "null" <|
+                expectParses ("false")
+        , test "null" <|
             \() ->
-                expectParses("null")
-      , test "true member" <|
+                expectParses ("null")
+        , test "true member" <|
             \() ->
-                expectParses("""{ "member": true }""")
-      , test "false member" <|
+                expectParses ("""{ "member": true }""")
+        , test "false member" <|
             \() ->
-                expectParses("""{ "member": false }""")
-      , test "null member" <|
+                expectParses ("""{ "member": false }""")
+        , test "null member" <|
             \() ->
-                expectParses("""{ "member": null }""")
-      , test "members" <|
+                expectParses ("""{ "member": null }""")
+        , test "members" <|
             \() ->
-                expectParses("""{ "member1": true, "member2": false }""")
-      , test "string" <|
+                expectParses ("""{ "member1": true, "member2": false }""")
+        , test "string" <|
             \() ->
-                expectParses("""{ "member": "Fred" }""")
-      , test "embedded quote" <|
+                expectParses ("""{ "member": "Fred" }""")
+        , test "embedded quote" <|
             \() ->
-              let 
-                -- as we're using embedded quotes in the pattern string
-                -- and we're taking it literally with """ then we need to
-                -- doubly escape the embedded quotes in the pattern
-                pattern =
-                    """{ "member": "Fred  \\"Bloggs\\""  }"""
-              in 
-                expectParses(pattern)
-      , test "array" <|
+                let
+                    -- as we're using embedded quotes in the pattern string
+                    -- and we're taking it literally with """ then we need to
+                    -- doubly escape the embedded quotes in the pattern
+                    pattern =
+                        """{ "member": "Fred  \\"Bloggs\\""  }"""
+                in
+                    expectParses (pattern)
+        , test "array" <|
             \() ->
-                expectParses("""{ "arr": [ 1, 2, 3 ] }""")
-      , test "emptyArray" <|
+                expectParses ("""{ "arr": [ 1, 2, 3 ] }""")
+        , test "emptyArray" <|
             \() ->
-                expectParses("""{ "arr": [ ] }""")
-      ]
+                expectParses ("""{ "arr": [ ] }""")
+        ]
+
 
 wildTests : Test
 wildTests =
     describe "From the wild"
         [ test "address book" <|
             \() ->
-                expectParses(addressBook)
-        ,  test "twitter" <|
+                expectParses (addressBook)
+        , test "twitter" <|
             \() ->
-                expectParses(twitter)
-        ,  test "Google maps" <|
+                expectParses (twitter)
+        , test "Google maps" <|
             \() ->
-                expectParses(googleMaps)
-        ,  test "Elm config" <|
+                expectParses (googleMaps)
+        , test "Elm config" <|
             \() ->
-                expectParses(elmConfig)
-        ,  test "Facebook" <|
+                expectParses (elmConfig)
+        , test "Facebook" <|
             \() ->
-                expectParses(facebook)
-        ,  test "github" <|
+                expectParses (facebook)
+        , test "github" <|
             \() ->
-                expectParses(github)
-
+                expectParses (github)
         ]
 
-{-|-}
+
+{-| -}
 all : Test
 all =
-  concat
-    [
-      basicTests
-    , wildTests
-    ]
+    concat
+        [ basicTests
+        , wildTests
+        ]
